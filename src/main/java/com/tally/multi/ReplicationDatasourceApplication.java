23e99d31a69108487bcec92702668761a376765d
ahn-sj <jaesa5221@gmail.com>
2024-08-06 19:26:13 +0900

    Init: Project Initial

---
+++ src/main/java/com/tally/multi/ReplicationDatasourceApplication.java
@@ -0,0 +1,13 @@
+package com.tally.multi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReplicationDatasourceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReplicationDatasourceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tally/multi/ReplicationDatasourceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tally.multi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReplicationDatasourceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
